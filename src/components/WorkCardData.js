3b5d20f4aa6bf638dd847e770d9106a2503fe352
chunyim <chunhinyim@gmail.com>
2023-11-12 22:08:56 -0500

    amended project part

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,5 +1,6 @@
 import seekverImg from "../assets/seekverImg.png";
 import catGallery from "../assets/catgallery.png";
+import foodbank from "../assets/Hackathon_foodbank.png";
 import wordGuessingGame from "../assets/wordguessinggame.png";
 
 const ProjectCardData = [
@@ -8,21 +9,28 @@ const ProjectCardData = [
     title: "Seekver Website -- MEAN Stack Project",
     text: "It is a dynamic and user-friendly platform designed to connect problem seekers with skilled solvers. The website will keep updating with the vision of becoming the ultimate destination for individuals seeking solutions and those equipped with the expertise to deliver them.",
     view: "https://seekver.netlify.app",
-    github: "https://github.com/chunyim/Seekver-Frontend"
+    github: "https://github.com/chunyim/Seekver-Frontend",
+  },
+  {
+    imgsrc: foodbank,
+    title: "WIMTACH 2-day Front-End Hackathon",
+    text: "A web and mobile application designed to facilitate food delivery between food banks and the community, while also offering a convenient search feature for finding suitable food bank providers. Key features include Responsive Design, Reusable UI/UX components, HTML5-qrcode and Chat Bot",
+    view: "https://github.com/hinilyu/foodbank",
+    github: "https://github.com/hinilyu/foodbank",
   },
   {
     imgsrc: catGallery,
     title: "Cat Gallery -- Front-End JS Project",
     text: "It is a gallery utilizing the cat API to fetch cat images, where users can select their favorite photos, and a maximum of 9 cat pictures will be saved.",
     view: "https://cat-gallery-chunhin.netlify.app",
-    github: "https://github.com/chunyim/catgallery"
+    github: "https://github.com/chunyim/catgallery",
   },
   {
     imgsrc: wordGuessingGame,
     title: "Word Guessing Game -- Front-End JS Project",
     text: "It is a word guessing game where users can make as many attempts as they want until they win!",
     view: "https://comp125-guessing-game.netlify.app/",
-    github: "https://github.com/chunyim/guessing-game"
+    github: "https://github.com/chunyim/guessing-game",
   },
 ];
 

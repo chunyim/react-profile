81267ecd23008b4155088346078f78d0a2a12ba8
chunyim <chunhinyim@gmail.com>
2024-02-13 09:23:31 -0500

    changed projects

--- src/components/Work.js
+++ src/components/Work.js
@@ -9,21 +9,6 @@ import OOPCardData from "./OOPCardData";
 const Work = () => {
   return (
     <div className="container">
-      <h1 className="project-heading">Web Projects</h1>
-      <div className="project-container">
-        {WorkCardData.map((val, ind) => {
-          return (
-            <WorkCard
-              key={ind}
-              title={val.title}
-              imgsrc={val.imgsrc}
-              text={val.text}
-              view={val.view}
-              github={val.github}
-            />
-          );
-        })}
-      </div>
       <h1 className="project-heading">OOP Projects</h1>
       <div className="OOPProject-container">
         {OOPCardData.map((val, ind) => {
@@ -34,6 +19,22 @@ const Work = () => {
               imgsrc={val.imgsrc}
               text={val.text}
               // view={val.view}
+              github={val.github}
+            />
+          );
+        })}
+      </div>
+
+      <h1 className="project-heading">Web Projects</h1>
+      <div className="project-container">
+        {WorkCardData.map((val, ind) => {
+          return (
+            <WorkCard
+              key={ind}
+              title={val.title}
+              imgsrc={val.imgsrc}
+              text={val.text}
+              view={val.view}
               github={val.github}
             />
           );
